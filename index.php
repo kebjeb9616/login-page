f4685e17cb25dd4c3302122ccefc75596ca9324b
kebjeb9616 <kebjeb91@gmail.com>
2022-08-28 10:41:07 +0700

    menambahkan index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+  </head>
+  <body>
+    <h2 class="title">MASUK</h2>
+    <form action="" method="get">
+      <input type="email" class="email" placeholder="email kamu" require>
+      <input type="password" class="pass" placeholder="password kamu" require>
+      <input type="password" class="conpass" placeholder="konfirmasi password kamu" require>
+      <input type="submit" value="">
+    </form>
+  </body>
+</html>
