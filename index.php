64f5ae23540d01ee6f40252dbeb9fa5647689f37
kebjeb9616 <kebjeb91@gmail.com>
2022-08-28 11:48:42 +0700

    menambahkan style.css dan merubah backgroud color

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
     <h2 class="title">MASUK</h2>
-    <form action="" method="get">
-      <input type="email" class="email" placeholder="email kamu" require>
-      <input type="password" class="pass" placeholder="password kamu" require>
-      <input type="password" class="conpass" placeholder="konfirmasi password kamu" require>
-      <input type="submit" value="">
-    </form>
+    <div class="form">
+     <form action="" method="get">
+       <input type="email" class="email" placeholder="email kamu" require>
+       <input type="password" class="pass" placeholder="password kamu" require>
+       <input type="password" class="conpass" placeholder="konfirmasi password kamu" require>
+       <input type="submit" value="">
+     </form>
+    </div>
   </body>
 </html>
